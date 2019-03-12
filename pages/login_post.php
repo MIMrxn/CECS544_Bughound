5cd54a7c0c638a61830f77cbc5e40df1e68a3e9f
Rob Marquez <robienallen@yahoo.com>
2019-03-11 21:59:55 -0700

    Login complete

---
+++ pages/login_post.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bughound</title>
+    </head>
+    <body>
+        <h2>
+            <!-- ADD YOUR DB INFO HERE -->
+            <?php
+				$con = mysqli_connect("localhost","root","","bughound_db");
+				
+				// Check connection
+				if (mysqli_connect_errno())
+				{
+					echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				}
+
+				$sql = "SELECT * FROM employees WHERE user_name = '".$_POST['username']."' AND password = '".$_POST['password']."'";
+				
+				if ($result = mysqli_query($con, $sql))
+				{
+					// Return the number of rows in result set
+					$rowcount=mysqli_num_rows($result);
+
+					if($rowcount > 0) {
+						header("Location: index.php");
+					}
+					else {
+						header("Location: login.php");
+					}
+					  
+					// Free result set
+					mysqli_free_result($result);
+				}
+
+				mysqli_close($con);
+			?>
+        </h2>
+    </body>
+</html>
