78a590e56c25db3c51d03eb7c56029142ced1692
MIMrxn <Fractal@Marvins-MacBook-Pro.local>
2019-03-13 22:39:58 -0700

    fixed index page and removed global search

---
+++ sql_files/tests.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees (first_name, last_name, user_name, password, position, group_num, is_reporter, user_level) VALUES ("Pip", "", "Redcar", "theatre44", "Manager", 10, 0, 5);
+
+INSERT INTO employees (first_name, last_name, user_name, password, position, group_num, is_reporter, user_level) VALUES ("Brutus", "", "Quintus", "younger85", "Tester", 3, 1, 1);
