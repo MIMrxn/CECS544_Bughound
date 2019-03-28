3fc6cdf86c9ec1a341e1d117b72dd526570135bc
MIMrxn <Fractal@Marvins-MacBook-Pro.local>
2019-03-27 17:58:16 -0700

    added report id to attachments

--- pages/create_report_post.php
+++ pages/create_report_post.php
@@ -44,7 +44,18 @@
 			
 			if(isset($_FILES)){
 				$has_attachments = 1;
-				$target_dir = "C:/xampp/htdocs/CECS544_Bughound/uploads/"; // change this directory to whatever you want the file to be saved on 
+                $file_root = $_SERVER['DOCUMENT_ROOT']; // root path
+                $current_page = basename(__FILE__); // current page name
+                $parent = basename(__DIR__); // parent folder name
+                $mid_path = str_replace('/'.$parent.'/'.$current_page, '', $_SERVER['REQUEST_URI']); // path after root path
+                $target_dir = $file_root . $mid_path . "/uploads/"; // target directory
+                // Make directory if doesn't exist with permissions
+                if(!is_dir($target_dir)) {
+                    mkdir($target_dir, 0755);
+                }
+                //echo "console.log('This is the file root: '".$file_root."' ')";
+                //echo "console.log('This is the curr file: '".$current_page."' ')";
+                //echo "console.log('This is the where to store file: '".$target_dir."' ')";
 				$target_file = $target_dir . basename($_FILES["attachments"]["name"]); // file name
 				$uploadOk = 1;
 				$file_name = basename($_FILES["attachments"]["name"]);
@@ -53,18 +64,22 @@
 				// Check if file already exists
 				if (file_exists($target_file)) {
 					echo "Sorry, file already exists.";
+                    //echo "console.log('Sorry, file already exists.');";
 					$uploadOk = 0;
 				}
 
 				// Check if $uploadOk is set to 0 by an error
 				if ($uploadOk == 0) {
 					echo "Sorry, your file was not uploaded.";
+                    echo "console.log('Sorry, file not uploaded.');";
 				// if everything is ok, try to upload file
 				} else {
 					if (move_uploaded_file($_FILES["attachments"]["tmp_name"], $target_file)) {
 						echo "The file ". basename( $_FILES["attachments"]["name"]). " has been uploaded.";
+                        //echo "console.log('File uploaded.');";
 					} else {
 						echo "Sorry, there was an error uploading your file.";
+                        //echo "console.log('Sorry, error.');";
 					}
 				}		
 				
@@ -73,7 +88,7 @@
 				$conn = new mysqli($servername, $username, $password);
 				mysqli_select_db($conn, "bughound_db");
 				$stmt = $conn->prepare("INSERT INTO attachments (file_name, file_type) VALUES (?, ?)");
-				$stmt->bind_param("ss", $file_name, $fileType);
+				$stmt->bind_param("ss", $target_file, $fileType);
 				$stmt->execute();
 			}
 			else {
@@ -126,6 +141,23 @@
             $stmt->bind_param("isissssisiisisiisissssi", $program_id, $report_type, $severity, $summary, $reproducible, $problem_description, $suggested_fix, $reported_by, $date_discovered, $area_id, $assigned_to, $status, $priority, $resolution, $resolution_version, $resolved_by, $date_resolved, $tested_by, $date_tested, $treat_deferred, $has_attachments, $comments, $is_visible);
             $stmt->execute();
             
+            if($has_attachments === 1) {
+                $sql_report = "SELECT report_id FROM bugs ORDER BY report_id DESC LIMIT 1";
+                $report_result = $conn->query($sql_report);
+                $report_row=$report_result->fetch_assoc();
+                //echo "console.log('This is the report id: ".$report_row['report_id']."');";
+
+                $sql_attach = "SELECT file_id FROM attachments ORDER BY file_id DESC LIMIT 1";
+                $attach_result = $conn->query($sql_attach);
+                $attach_row=$attach_result->fetch_assoc();
+                //echo "console.log('This is the att id: ".$attach_row['file_id']."');";
+
+                $update_sql = "UPDATE attachments SET report_id = '".$report_row['report_id']."' WHERE file_id = '".$attach_row['file_id']."'";
+                //echo "console.log('This is the sql: ".$update_sql."');";
+
+                $conn->query($update_sql);
+            }
+
             $stmt->close();
             $conn->close();
             
